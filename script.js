495175fe985f2af16a259e4823b9d83fc167067e
angeliwanov <angel.iwanov@gmail.com>
2022-12-05 13:42:58 +0200

    api buttons loader

--- script.js
+++ script.js
@@ -1,1 +1,67 @@
-console.log('success')
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+let apiQuotes = [];
+
+// Show Loader
+function laoding() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
+// Show New Quotes
+function newQuote() {
+    laoding()
+    // Pick a random quote from array
+    const quote = apiQuotes[Math.floor(Math.random()*apiQuotes.length)];
+    // Check if author is unknown
+    if (!quote.author) {
+        authorText.textContent = 'Unknown';
+    } else {
+        authorText.textContent = quote.author;
+    }
+    // Check if quote is too long to determine styling
+    if (quote.text.length > 120) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+    quoteText.textContent = quote.text;
+    complete();
+}
+
+// Get Quotes from Api
+async function getQuotes() {
+    laoding();
+    const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+// Tweet Quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank')
+}
+
+// Add EventListeners
+newQuoteBtn.addEventListener ('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+// on Load
+getQuotes();
